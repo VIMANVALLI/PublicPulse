d383305b1925c39692653f60af7ee258a9b4d269
TANAALA VIMAN VALLI <viman956@gmail.com>
2026-09-02 16:43:13 +0530

    Update CORS allowed origins for deployment

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://publicpulse-569.onrender.com",
+        "https://publicpulse-vv.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
